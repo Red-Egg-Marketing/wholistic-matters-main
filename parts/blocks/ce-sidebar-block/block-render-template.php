cfed963e1de936c3150bf2805478eb12c2f171ee
Zach <zach@Zachs-MacBook-Pro.local>
2026-08-28 10:14:17 -0600

    Re-factor CE sidebar buttons with repeater, add text button style

--- parts/blocks/ce-sidebar-block/block-render-template.php
+++ parts/blocks/ce-sidebar-block/block-render-template.php
@@ -7,8 +7,6 @@
  */
 
 $content = get_field('ce_sidebar_content', 'option');
-$button = get_field('ce_sidebar_button', 'option');
-
 ?>
 
 <div class="courses-content-right">
@@ -18,14 +16,26 @@ $button = get_field('ce_sidebar_button', 'option');
             <?php echo wp_kses_post($content); ?>
         <?php endif; ?>
 
-        <?php if ($button) : ?>
-            <a
-                href="<?php echo esc_url($button["url"]); ?>"
-                class="primary-button"
-                target="<?php echo esc_attr($button["target"]); ?>"
-            >
-                <?php echo esc_html($button["title"]); ?>
-            </a>
+        <?php if( have_rows('ce_sidebar_repeater', 'option') ): ?>
+            <?php while( have_rows('ce_sidebar_repeater', 'option') ): the_row(); ?>
+
+                <?php
+                $sidebar_button = get_sub_field('ce_sidebar_button','option');
+                $sidebar_button_style = get_sub_field('ce_sidebar_button_style','option');
+                $sidebar_button_class = ( $sidebar_button_style === 'primary_button' ) ? 'primary-button' : 'text-button';
+                ?>
+
+                <?php if ($sidebar_button) : ?>
+                <a
+                    href="<?php echo esc_url($sidebar_button["url"]); ?>"
+                    class="<?php echo $sidebar_button_class ?>"
+                    target="<?php echo esc_attr($sidebar_button["target"]); ?>"
+                >
+                    <?php echo esc_html($sidebar_button["title"]); ?>
+                </a>
+                <?php endif; ?>
+
+            <?php endwhile; ?>
         <?php endif; ?>
 
     </div>
